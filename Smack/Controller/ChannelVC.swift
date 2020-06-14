ef51cb7ce854883721bcc733d8bc593cda5e6e1c
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-06-14 18:36:22 -0400

    Add slide menu with SWReveal

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChannelVC.swift
 //  Smack
 //
 //  Created by Kazi Jawad on 6/14/20.
@@ -8,13 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ChannelVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 100
     }
-
-
 }
-
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Kazi Jawad on 6/14/20.
+//  Copyright © 2020 Kazi Jawad. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+}
