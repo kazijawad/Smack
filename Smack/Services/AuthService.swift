f0eb725c90354157e56f35a009ac476031066b46
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-06-20 14:44:10 -0400

    Add user login

--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService {
     static let instance = AuthService()
@@ -44,19 +45,44 @@ class AuthService {
     func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
         let lowerCaseEmail = email.lowercased()
         
-        let headers: HTTPHeaders = [
-            "Content-Type": "application/json; charset=utf-8"
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password,
         ]
+        
+        AF.request(REGISTER_URL, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADERS).responseString { (response) in
+            switch response.result {
+            case .success( _):
+                completion(true)
+            case .failure(let error):
+                completion(false)
+                debugPrint(error as Any)
+            }
+        }
+    }
+    
+    func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
         
         let body: [String: Any] = [
             "email": lowerCaseEmail,
             "password": password,
         ]
         
-        AF.request(REGISTER_URL, method: .post, parameters: body, encoding: JSONEncoding.default, headers: headers).responseString { (response) in
+        AF.request(LOGIN_URL, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADERS).responseJSON { (response) in
             switch response.result {
             case .success( _):
-                completion(true)
+                guard let data = response.data else { return }
+                do {
+                    let json = try JSON(data: data)
+                    self.userEmail = json["user"].stringValue
+                    self.authToken = json["token"].stringValue
+                    self.isLoggedIn = true
+                    completion(true)
+                } catch let error {
+                    completion(false)
+                    debugPrint(error as Any)
+                }
             case .failure(let error):
                 completion(false)
                 debugPrint(error as Any)
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -7,12 +7,14 @@
 //
 
 import Foundation
+import Alamofire
 
 typealias CompletionHandler = (_ Success: Bool) -> ()
 
 // URLs
 let BASE_URL = "http://localhost:3005/v1/"
 let REGISTER_URL = "\(BASE_URL)account/register"
+let LOGIN_URL = "\(BASE_URL)account/login"
 
 // Segues
 let TO_LOGIN = "toLogin"
@@ -23,3 +25,8 @@ let UNWIND_TO_CHANNEL = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+// Headers
+let HEADERS: HTTPHeaders = [
+    "Content-Type": "application/json; charset=utf-8"
+]
