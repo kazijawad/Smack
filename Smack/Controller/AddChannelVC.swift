1423ca9969d86bd689aeb12678e8452d295a5041
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-06-21 19:47:41 -0400

    Create Add Channel Modal

---
+++ Smack/Controller/AddChannelVC.swift
@@ -0,0 +1,40 @@
+//
+//  AddChannelVC.swift
+//  Smack
+//
+//  Created by Kazi Jawad on 6/21/20.
+//  Copyright © 2020 Kazi Jawad. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+    @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var descriptionTxt: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func createChannelPressed(_ sender: Any) {
+    
+    }
+    
+    func setupView() {
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+        
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "Name", attributes: [NSAttributedString.Key.foregroundColor: smackPurplePlaceholder])
+        descriptionTxt.attributedPlaceholder = NSAttributedString(string: "Description", attributes: [NSAttributedString.Key.foregroundColor: smackPurplePlaceholder])
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+}
