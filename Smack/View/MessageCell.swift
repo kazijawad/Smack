b3f43178560dbff9b503ef1a3913a79693524a54
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-07-02 19:30:11 -0400

    Display timestamp

--- Smack/View/MessageCell.swift
+++ Smack/View/MessageCell.swift
@@ -23,5 +23,20 @@ class MessageCell: UITableViewCell {
         userNameLabel.text = message.userName
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
         messageBodyLabel.text = message.message
+        
+        guard var isoDate = message.timestamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timestampLabel.text = finalDate
+        }
     }
 }
